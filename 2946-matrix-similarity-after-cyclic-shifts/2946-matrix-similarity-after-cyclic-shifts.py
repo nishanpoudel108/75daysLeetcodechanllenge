a890659a1fd1df20c27da3a6c28591ac94b79693
Nishan Poudel <157877328+nishanpoudel108@users.noreply.github.com>
2026-03-27 19:22:37 +0545

    Time: 9 ms (12.5%), Space: 12.3 MB (97.5%) - LeetHub

---
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def areSimilar(self, mat, k):
+        m, n = len(mat), len(mat[0])
+        k %= n
+        
+        for i in range(m):
+            for j in range(n):
+                if i % 2 == 0:
+                    # even row → left shift
+                    if mat[i][j] != mat[i][(j + k) % n]:
+                        return False
+                else:
+                    # odd row → right shift
+                    if mat[i][j] != mat[i][(j - k + n) % n]:
+                        return False
+        
+        return True
