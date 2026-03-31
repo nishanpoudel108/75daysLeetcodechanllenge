a98f50cfe8601e0d044a4bc9a6646b75eab6f49c
Nishan Poudel <157877328+nishanpoudel108@users.noreply.github.com>
2026-03-31 21:01:51 +0545

    Time: 25 ms (12.99%), Space: 15.6 MB (87.69%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.py
@@ -0,0 +1,25 @@
+class MinStack(object):
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val):
+        self.stack.append(val)
+        
+        # If min_stack is empty OR new val is smaller/equal
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+
+    def pop(self):
+        # If popped element is the current minimum, pop from min_stack too
+        if self.stack[-1] == self.min_stack[-1]:
+            self.min_stack.pop()
+        
+        self.stack.pop()
+
+    def top(self):
+        return self.stack[-1]
+
+    def getMin(self):
+        return self.min_stack[-1]
