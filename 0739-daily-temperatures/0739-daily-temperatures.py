c09096168316d1f52ae5aa5e9596ffde1eacb26a
Nishan Poudel <157877328+nishanpoudel108@users.noreply.github.com>
2026-04-02 09:51:53 +0545

    Time: 145 ms (94.09%), Space: 26.9 MB (68.12%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+        n = len(temperatures)
+        result = [0] * n
+        stack = []  # will store indices
+        
+        for i in range(n):
+            # resolve previous colder days
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                prev_index = stack.pop()
+                result[prev_index] = i - prev_index
+            
+            stack.append(i)
+        
+        return result
