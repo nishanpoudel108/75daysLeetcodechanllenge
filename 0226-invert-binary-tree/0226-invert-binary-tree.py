a8215bea634edab898d7b49af803c551daf1c1c4
Nishan Poudel <157877328+nishanpoudel108@users.noreply.github.com>
2026-04-14 20:08:46 +0545

    Time: 0 ms (100%), Space: 12.3 MB (97.45%) - LeetHub

---
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+class Solution(object):
+    def invertTree(self, root):
+        if not root:
+            return None
+        
+        queue = deque([root])
+        
+        while queue:
+            node = queue.popleft()
+            
+            # Swap children
+            node.left, node.right = node.right, node.left
+            
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        
+        return root
